455bf7585a15b8e9c5377013df7463f3c8e37fb4
Biswayan Paul <biswayanpaul2015@gmail.com>
2024-09-23 02:05:23 +0530

    changed

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { WebSocketServer, WebSocket } from "ws";
 import { parse } from "url";
 
 const app = express();
-const httpServer = app.listen(8080, () => {
+
+const PORT = process.env.PORT || 8080;
+
+const httpServer = app.listen(PORT, () => {
   console.log("HTTP server listening on port 8080");
 });
 
@@ -67,4 +70,4 @@ wss.on("connection", function connection(ws, req) {
   );
 });
 
-console.log("WebSocket server is running on ws://localhost:8080");
+console.log(`WebSocket server is running on ws://localhost:${PORT}`);
